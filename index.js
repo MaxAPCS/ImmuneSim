c495162ddc89ae9f0bfe74fc1d718b12f64692df
MAXWELL LIU <MALIU2@s.sfusd.edu>
2023-11-07 09:29:40 -0800

    wor

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const canvasSize = 800;
-const TPS = 20;
+const TPS = 120
 
 const entities = [];
-let vectorField = Array.from(new Array(canvasSize/100)).map(() => []);
+let bacVectorField = [];
+for (let i = 0; i < canvasSize/100; i++) bacVectorField[i] = []
+
+Number.prototype.normalize = function(low, high) {
+	if (this < low) return this + (high - low) * Math.ceil((low - this) / (high - low))
+	if (this >= high) return (this - low) % (high - low) + low;
+	return this;
+}
 
 class Organism {
 	constructor(startpos) {
@@ -11,19 +18,15 @@ class Organism {
 	}
 
 	tick() {
-		let tpos = p5.Vector.add(this.pos, this.vel)
-		if (tpos.x < 0 || tpos.x >= canvasSize) {
-			this.vel.mult(-1, 1)
-		} else if (tpos.y < 0 || tpos.y >= canvasSize) {
-			this.vel.mult(1, -1)
-		}
 		this.pos.add(this.vel.div(TPS))
+		this.pos.x = this.pos.x.normalize(0, canvasSize)
+		this.pos.y = this.pos.y.normalize(0, canvasSize)
 	}
 }
 
 class Bacteria extends Organism {
 	tick() {
-		this.vel = vectorField[Math.floor(this.pos.y/100)][Math.floor(this.pos.x/100)]
+		this.vel = bacVectorField[Math.floor(this.pos.y/100)][Math.floor(this.pos.x/100)]
 		super.tick()
 	}
 
@@ -34,10 +37,45 @@ class Bacteria extends Organism {
 	}
 }
 
+class RedBlood extends Organism {
+	constructor() {
+		super(createVector(5, Math.floor(Math.random()*canvasSize)))
+	}
+
+	tick() {
+		this.vel = createVector(this.vel.x + Math.random()*80 - 20, Math.random()*5-2.5)
+		super.tick()
+	}
+
+	draw() {
+		stroke(color(220, 20, 20))
+		strokeWeight(12)
+		point(this.pos.x, this.pos.y)
+	}
+}
+
+class WhiteBlood extends Organism {
+	tick() {
+		// this.vel = p5.Vector.mult(vectorField[Math.floor(this.pos.y/100)][Math.floor(this.pos.x/100)], -1)
+		super.tick()
+	}
+
+	draw() {
+		stroke(color(220, 220, 220))
+		strokeWeight(15)
+		point(this.pos.x, this.pos.y)
+	}
+}
+
 function setup() {
   createCanvas(canvasSize-1, canvasSize-1);
   frameRate(60)
-  for (let i = 0; i < 2; i++) entities.push(new Bacteria())
+  for (let i = 0; i < 40; i++) entities.push(new RedBlood())
+  for (let i = 0; i < 10; i++) entities.push(new WhiteBlood())
+}
+
+function mouseClicked() {
+	entities.push(new Bacteria(createVector(mouseX, mouseY)))
 }
 
 let lastTick = 0;
@@ -49,18 +87,26 @@ function draw() {
   	for (let r = 0; r < canvasSize/100; r++) {
   		for (let c = 0; c < canvasSize/100; c++) {
   			const center = createVector(c*100+50, r*100+50)
-  			const forceparts = entities.filter(e => 
-  				r <= e.pos.y/100 && e.pos.y/100 < r+1 &&
-  				c <= e.pos.x/100 && e.pos.x/100 < c+1)
-  			.map(e => e.pos.sub(center))
-  			vectorField[r][c] = forceparts.length === 0 ? createVector() : forceparts.reduce((acc, it) => acc.add(it)).div(forceparts.length)
+  			const forcepartsbac = entities
+				.filter(e => r !== Math.floor(e.pos.y/100) || c !== Math.floor(e.pos.x/100))
+  				.map(e => e instanceof RedBlood ? p5.Vector.sub(e.pos, center) : e instanceof WhiteBlood ? p5.Vector.sub(center, e.pos) : null)
+				.filter(v => !!v)
+  			bacVectorField[r][c] = forcepartsbac.length === 0 ? createVector() : forcepartsbac.reduce((acc, it) => acc.add(it)).div(forcepartsbac.length)
   		}
   	}
 
-  	for (let organism of entities) {
+	for (let organism of entities) {
   		organism.tick();
   	}
   }
+
+//   for (let a = 0; a < vectorField.length; a++)
+//   for (let b = 0; b < vectorField[a].length; b++) {
+// 	stroke(color(vectorField[a][b].mag(), 5, 5))
+// 	strokeWeight(5)
+// 	line(b*100+50, a*100+50, b*100+50+vectorField[a][b].x, a*100+50+vectorField[a][b].y)
+//   }
+
   for (let organism of entities) {
   	organism.draw();
   }
