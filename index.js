b244e47f91c01b4e83033243c2e1a45997ca685b
MAXWELL LIU <MALIU2@s.sfusd.edu>
2023-11-09 10:10:29 -0800

    i am a human

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ function chunksInRange(pos, rad) {
 	let chunks = []
 	for (let r = 0; r*chunkSize <= height; r++) {
 		for (let c = 0; c*chunkSize <= width; c++) {
-			let [y, x] = [r * chunkSize, c * chunkSize]
-			// x, y is top left
+			let [y, x] = [r * chunkSize, c * chunkSize] // x, y is top left
 			if (!(x <= pos.x + rad && x + chunkSize > pos.x - rad && y <= pos.y + rad && y + chunkSize > pos.y - rad)) continue
 			chunks.push([r, c])
 		}
@@ -38,9 +37,7 @@ class Organism {
 	tick() {
 		if (this.vel.magSq() > 0) {
 			this.vel.limit(this.topspeed)
-			this.pos.add(p5.Vector.mult(this.vel, elapsedTicks()).limit(this.topspeed))
-			// this.pos.x = Math.round(this.pos.x * 100)/100
-			// this.pos.y = Math.round(this.pos.y * 100)/100
+			this.pos.add(p5.Vector.mult(this.vel, elapsedTicks()))
 		}
 		this.updatePos()
 	}
@@ -52,7 +49,7 @@ class Organism {
 		if (this.r === r && this.c === c) return
 		if (entities[r] === undefined) entities[r] = []
 		if (entities[r][c] === undefined) entities[r][c] = new Set()
-		if (this.r && this.c) this.kill()
+		if (this.r !== undefined && this.c !== undefined) this.kill()
 		this.r = r; this.c = c;
 		entities[r][c].add(this)
 	}
@@ -78,17 +75,8 @@ class Bacteria extends Organism {
 			entities[this.r][this.c].add(new Bacteria(this.pos))
 			return
 		}
-		if (this.target instanceof Organism ? !entities[this.target.r][this.target.c].has(this.target) : (this.target+=elapsedTicks()) >= 0) {
-			let toCheck = []
-			for (let [r, c] of chunksInRange(this.pos, this.sight))
-				if (entities[r] && entities[r][c]) toCheck = toCheck.concat([...entities[r][c]])
-			let tar = toCheck
-				.filter(e => e instanceof RedBlood)
-				.map(e => [e, p5.Vector.dist(e.pos, this.pos)])
-				.filter(([_, dist]) => dist < this.sight)
-				.sort(([_, dist1], [__, dist2]) => dist1 - dist2)[0]
-			this.target = tar ? tar[0] : -100;
-		}
+		if (this.target instanceof Organism ? !entities[this.target.r][this.target.c].has(this.target) : (this.target+=elapsedTicks()) >= 0)
+			this.target = fetchClosestTarget(this.sight) ?? -100;
 		if (this.target instanceof Organism) this.vel = p5.Vector.sub(this.target.pos, this.pos)
 		else this.vel.add(p5.Vector.random2D().setMag(0.1)).limit(1)
 		super.tick()
@@ -105,8 +93,20 @@ class Bacteria extends Organism {
 		if (this.target instanceof Organism && p5.Vector.dist(this.pos, this.target.pos) < 6+this.size/2) {
 			this.target.kill()
 			this.size = Math.min(this.size+40/this.size, 50)
-			this.target = -10*(this.size**2)
+			this.target = -(this.size**2)
 		}
+	}
+
+	fetchClosestTarget(range) { // For Component C, i
+		let toCheck = []
+		for (let [r, c] of chunksInRange(this.pos, range))
+			if (entities[r] && entities[r][c]) toCheck = toCheck.concat([...entities[r][c]])
+		let tar = toCheck
+			.filter(e => e instanceof RedBlood)
+			.map(e => [e, p5.Vector.dist(e.pos, this.pos)])
+			.filter(([_, dist]) => dist < range)
+			.sort(([_, dist1], [__, dist2]) => dist1 - dist2)[0]
+		return tar ? tar[0] : null
 	}
 }
 
@@ -131,7 +131,7 @@ class WhiteBlood extends Organism {
 	}
 
 	tick() {
-		if (!this.target || !entities[this.target.r][this.target.c].has(this.target)) {
+		if (!this.target || !entities[this.target.r][this.target.c].has(this.target) || p5.Vector.dist(this.pos, this.target.pos) > this.sight*2) {
 			let toCheck = []
 			for (let [r, c] of chunksInRange(this.pos, this.sight))
 				if (entities[r] && entities[r][c]) toCheck = toCheck.concat([...entities[r][c]])
@@ -155,9 +155,9 @@ class WhiteBlood extends Organism {
 	updatePos() {
 		super.updatePos()
 		let toCheck = []
-		for (let [r, c] of chunksInRange(this.pos, 50))
+		for (let [r, c] of chunksInRange(this.pos, 26))
 			if (entities[r] && entities[r][c]) toCheck = toCheck.concat([...entities[r][c]])
-		toCheck = toCheck.filter(e => (e instanceof Bacteria) && (p5.Vector.dist(e.pos, this.pos) <= 7.5 + e.size/2))
+		toCheck = toCheck.filter(e => (e instanceof Bacteria) && (p5.Vector.dist(e.pos, this.pos) <= e.size/2))
 		for (let e of toCheck) e.kill()
 	}
 }
@@ -169,11 +169,11 @@ function setup() {
 	for (let i = 0; i < 5; i++) new WhiteBlood()
 }
 
-function windowResized() {
-	resizeCanvas(windowWidth, windowHeight);
-}
+function windowResized() {resizeCanvas(windowWidth, windowHeight)}
 
+let hasClicked = false
 function mouseClicked() {
+	hasClicked = true
 	new Bacteria(createVector(mouseX, mouseY))
 }
 
@@ -191,8 +191,31 @@ function draw() {
 		for (let organism of negativePriorityTickList) organism.tick()
 		lastTick = now;
 	}
-	
 	for (let chunk of entities.flat(2))
 	for (let organism of chunk)
 		organism.draw();
+	if (!hasClicked) {
+		noStroke()
+		fill(0xfff)
+		textSize(65);
+		textAlign(CENTER, CENTER)
+		text("Click to spawn bacteria!", width/2, height/2)
+		noFill()
+	}
+}
+
+function chunkDebug() {
+	noStroke()
+	const mp = createVector(mouseX, mouseY);
+	fill(10, 10, 150, 150)
+	let [r,c] = getChunkPos(mp)
+	rect(c*chunkSize, r*chunkSize, chunkSize, chunkSize)
+	fill(10, 150, 150, 150)
+	circle(mp.x, mp.y, 50*2)
+	fill(150, 10, 150, 150)
+	for (let [r, c] of chunksInRange(mp, 50))
+	rect(c*chunkSize, r*chunkSize, chunkSize, chunkSize)
+	fill(0xfff)
+	if (entities[r] && entities[r][c]) text(`Bacteria: ${[...entities[r][c]].filter(e => e instanceof Bacteria).length}\nTotal: ${entities[r][c].size}`, mp.x, mp.y)
+	noFill()
 }
