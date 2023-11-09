11c7a7bb4d70c519e8f7e951d28c20b554a80379
Max L <maliu2@s.sfusd.edu>
2023-11-08 23:44:25 -0800

    add everything

--- index.js
+++ index.js
@@ -1,52 +1,121 @@
-const canvasSize = 800;
-const TPS = 120
+const chunkSize = 100;
+const TPS = 20
 
 const entities = [];
-let bacVectorField = [];
-for (let i = 0; i < canvasSize/100; i++) bacVectorField[i] = []
 
 Number.prototype.normalize = function(low, high) {
 	if (this < low) return this + (high - low) * Math.ceil((low - this) / (high - low))
 	if (this >= high) return (this - low) % (high - low) + low;
 	return this;
 }
+const getChunkPos = (pos) => [Math.floor(pos.y/chunkSize), Math.floor(pos.x/chunkSize)]
+function chunksInRange(pos, rad) {
+	let chunks = []
+	for (let r = 0; r*chunkSize <= height; r++) {
+		for (let c = 0; c*chunkSize <= width; c++) {
+			let [y, x] = [r * chunkSize, c * chunkSize]
+			// x, y is top left
+			if (!(x <= pos.x + rad && x + chunkSize > pos.x - rad && y <= pos.y + rad && y + chunkSize > pos.y - rad)) continue
+			chunks.push([r, c])
+		}
+	}
+	return chunks;
+}
+function elapsedTicks() {
+	let pt = TPS * (Date.now() - lastTick)/1000
+	if (pt > 1.5) console.warn(`Tick Elapse trigger exceeded! (${pt-1} Ticks Behind)`)
+	return pt
+}
 
 class Organism {
 	constructor(startpos) {
-		this.pos = startpos ?? createVector(Math.floor(Math.random()*canvasSize), Math.floor(Math.random()*canvasSize))
+		this.pos = startpos ?? createVector(Math.floor(Math.random()*width), Math.floor(Math.random()*height))
+		this.updatePos()
 		this.vel = createVector()
+		this.topspeed = 0.5
+	}
+	
+	tick() {
+		if (this.vel.magSq() > 0) {
+			this.vel.limit(this.topspeed)
+			this.pos.add(p5.Vector.mult(this.vel, elapsedTicks()).limit(this.topspeed))
+			// this.pos.x = Math.round(this.pos.x * 100)/100
+			// this.pos.y = Math.round(this.pos.y * 100)/100
+		}
+		this.updatePos()
+	}
+	
+	updatePos() {
+		this.pos.x = this.pos.x.normalize(0, width)
+		this.pos.y = this.pos.y.normalize(0, height)
+		let [r, c] = getChunkPos(this.pos)
+		if (this.r === r && this.c === c) return
+		if (entities[r] === undefined) entities[r] = []
+		if (entities[r][c] === undefined) entities[r][c] = new Set()
+		if (this.r && this.c) this.kill()
+		this.r = r; this.c = c;
+		entities[r][c].add(this)
 	}
 
-	tick() {
-		this.pos.add(this.vel.div(TPS))
-		this.pos.x = this.pos.x.normalize(0, canvasSize)
-		this.pos.y = this.pos.y.normalize(0, canvasSize)
+	kill() {
+		if (!entities[this.r][this.c].delete(this)) console.error(`Entity @${this.pos} not in expected chunk [${this.r}][${this.c}] (not killed)`)
 	}
 }
 
 class Bacteria extends Organism {
+	constructor(p) {
+		super(p)
+		this.topspeed = 1.5
+		this.sight = 150
+		this.target = -10
+		this.size = 20
+	}
+	
 	tick() {
-		this.vel = bacVectorField[Math.floor(this.pos.y/100)][Math.floor(this.pos.x/100)]
+		if (this.size >= 50) { // mitosis
+			this.kill()
+			entities[this.r][this.c].add(new Bacteria(this.pos))
+			entities[this.r][this.c].add(new Bacteria(this.pos))
+			return
+		}
+		if (this.target instanceof Organism ? !entities[this.target.r][this.target.c].has(this.target) : (this.target+=elapsedTicks()) >= 0) {
+			let toCheck = []
+			for (let [r, c] of chunksInRange(this.pos, this.sight))
+				if (entities[r] && entities[r][c]) toCheck = toCheck.concat([...entities[r][c]])
+			let tar = toCheck
+				.filter(e => e instanceof RedBlood)
+				.map(e => [e, p5.Vector.dist(e.pos, this.pos)])
+				.filter(([_, dist]) => dist < this.sight)
+				.sort(([_, dist1], [__, dist2]) => dist1 - dist2)[0]
+			this.target = tar ? tar[0] : -100;
+		}
+		if (this.target instanceof Organism) this.vel = p5.Vector.sub(this.target.pos, this.pos)
+		else this.vel.add(p5.Vector.random2D().setMag(0.1)).limit(1)
 		super.tick()
 	}
-
+	
 	draw() {
-		stroke(color(20, 255, 20))
-		strokeWeight(20)
+		stroke(color(20, this.target instanceof Organism ? 255 : 180, 20))
+		strokeWeight(this.size)
 		point(this.pos.x, this.pos.y)
+	}
+
+	updatePos() {
+		super.updatePos()
+		if (this.target instanceof Organism && p5.Vector.dist(this.pos, this.target.pos) < 6+this.size/2) {
+			this.target.kill()
+			this.size = Math.min(this.size+40/this.size, 50)
+			this.target = -10*(this.size**2)
+		}
 	}
 }
 
 class RedBlood extends Organism {
-	constructor() {
-		super(createVector(5, Math.floor(Math.random()*canvasSize)))
-	}
-
 	tick() {
-		this.vel = createVector(this.vel.x + Math.random()*80 - 20, Math.random()*5-2.5)
+		this.vel.add(Math.random() - 0.4, Math.random()/2-1/4)
 		super.tick()
 	}
-
+	
 	draw() {
 		stroke(color(220, 20, 20))
 		strokeWeight(12)
@@ -55,59 +124,75 @@ class RedBlood extends Organism {
 }
 
 class WhiteBlood extends Organism {
-	tick() {
-		// this.vel = p5.Vector.mult(vectorField[Math.floor(this.pos.y/100)][Math.floor(this.pos.x/100)], -1)
-		super.tick()
+	constructor(p) {
+		super(p)
+		this.topspeed = 1.25
+		this.sight = 300
 	}
 
+	tick() {
+		if (!this.target || !entities[this.target.r][this.target.c].has(this.target)) {
+			let toCheck = []
+			for (let [r, c] of chunksInRange(this.pos, this.sight))
+				if (entities[r] && entities[r][c]) toCheck = toCheck.concat([...entities[r][c]])
+			let tar = toCheck
+				.filter(e => e instanceof Bacteria)
+				.map(e => [e, p5.Vector.dist(e.pos, this.pos)])
+				.filter(([_, dist]) => dist <= this.sight)
+				.sort(([_, dist1], [__, dist2]) => dist1 - dist2)[0]
+			this.target = tar ? tar[0] : null;
+		}
+		this.vel = this.target ? p5.Vector.sub(this.target.pos, this.pos) : createVector()
+		super.tick()
+	}
+	
 	draw() {
 		stroke(color(220, 220, 220))
 		strokeWeight(15)
 		point(this.pos.x, this.pos.y)
 	}
+
+	updatePos() {
+		super.updatePos()
+		let toCheck = []
+		for (let [r, c] of chunksInRange(this.pos, 50))
+			if (entities[r] && entities[r][c]) toCheck = toCheck.concat([...entities[r][c]])
+		toCheck = toCheck.filter(e => (e instanceof Bacteria) && (p5.Vector.dist(e.pos, this.pos) <= 7.5 + e.size/2))
+		for (let e of toCheck) e.kill()
+	}
 }
 
 function setup() {
-  createCanvas(canvasSize-1, canvasSize-1);
-  frameRate(60)
-  for (let i = 0; i < 40; i++) entities.push(new RedBlood())
-  for (let i = 0; i < 10; i++) entities.push(new WhiteBlood())
+	createCanvas(windowWidth, windowHeight);
+	frameRate(60)
+	for (let i = 0; i < 100; i++) new RedBlood()
+	for (let i = 0; i < 5; i++) new WhiteBlood()
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
 }
 
 function mouseClicked() {
-	entities.push(new Bacteria(createVector(mouseX, mouseY)))
+	new Bacteria(createVector(mouseX, mouseY))
 }
 
-let lastTick = 0;
+let lastTick = Date.now();
 function draw() {
-  background(0)
-  let now = Date.now();
-  if (now - lastTick > 1/TPS) {
-  	lastTick = now;
-  	for (let r = 0; r < canvasSize/100; r++) {
-  		for (let c = 0; c < canvasSize/100; c++) {
-  			const center = createVector(c*100+50, r*100+50)
-  			const forcepartsbac = entities
-				.filter(e => r !== Math.floor(e.pos.y/100) || c !== Math.floor(e.pos.x/100))
-  				.map(e => e instanceof RedBlood ? p5.Vector.sub(e.pos, center) : e instanceof WhiteBlood ? p5.Vector.sub(center, e.pos) : null)
-				.filter(v => !!v)
-  			bacVectorField[r][c] = forcepartsbac.length === 0 ? createVector() : forcepartsbac.reduce((acc, it) => acc.add(it)).div(forcepartsbac.length)
-  		}
-  	}
-
-	for (let organism of entities) {
-  		organism.tick();
-  	}
-  }
-
-//   for (let a = 0; a < vectorField.length; a++)
-//   for (let b = 0; b < vectorField[a].length; b++) {
-// 	stroke(color(vectorField[a][b].mag(), 5, 5))
-// 	strokeWeight(5)
-// 	line(b*100+50, a*100+50, b*100+50+vectorField[a][b].x, a*100+50+vectorField[a][b].y)
-//   }
-
-  for (let organism of entities) {
-  	organism.draw();
-  }
+	background(0)
+	let now = Date.now();
+	if (now - lastTick >= 1000/TPS) {
+		let negativePriorityTickList = []
+		for (let chunk of entities.flat(2))
+		for (let organism of chunk) {
+			if (organism instanceof Bacteria) organism.tick()
+			else negativePriorityTickList.push(organism)
+		}
+		for (let organism of negativePriorityTickList) organism.tick()
+		lastTick = now;
+	}
+	
+	for (let chunk of entities.flat(2))
+	for (let organism of chunk)
+		organism.draw();
 }
